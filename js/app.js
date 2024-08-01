e39aa2439adc01b360b83609f7168e49347af3cb
Александр <kadweb.dev@gmail.com>
2024-08-02 00:32:49 +0300

    commit

--- js/app.js
+++ js/app.js
@@ -71,7 +71,88 @@ function stepsScroll() {
     }
 }
 
+function possibleTabs() {
+	const possibleBlock = document.querySelector('.possible__main.desktop')
+	const tabs = possibleBlock.querySelectorAll('.possible-tab')
+	const contentBlocks = possibleBlock.querySelectorAll('.possible-item')
+
+	tabs[0].classList.add('active')
+	contentBlocks[0].classList.add('active')
+
+	tabs.forEach(function (tab) {
+		tab.addEventListener('click', function () {
+			tabs.forEach(function (tab) {
+				tab.classList.remove('active')
+			})
+
+			contentBlocks.forEach(function (block) {
+				block.classList.remove('active')
+			})
+
+			const tabIndex = Array.from(tabs).indexOf(tab)
+			tab.classList.add('active')
+			contentBlocks[tabIndex].classList.add('active')
+		})
+	})
+}
+
+function possibleCollapse() {
+	const possibleBlock = document.querySelector('.possible__main.mobile')
+	const triggerBtns = possibleBlock.querySelectorAll('.possible-tab')
+
+	triggerBtns.forEach(function (btn) {
+		btn.addEventListener('click', function () {
+			let currentBlock = btn.nextElementSibling
+
+			if (btn.classList.contains('active')) {
+				currentBlock.style.height = ''
+			} else {
+				currentBlock.style.height = currentBlock.scrollHeight + 80 + 'px'
+			}
+
+			btn.classList.toggle('active')
+			currentBlock.classList.toggle('active')
+		})
+	})
+}
+
+function reviewSwiper() {
+    const swiperEl = document.querySelector('.reviews-swiper')
+
+    if (swiperEl) {
+        new Swiper(swiperEl, {
+            spaceBetween: 30,
+			loop: true,
+			slidesPerView: 'auto',
+            navigation: {
+                nextEl: '.reviews-btn__next'
+            },
+            speed: 600
+        })
+    }
+}
+
+function collapse() {
+	const triggers = document.querySelectorAll('.faq-question')
+
+	triggers.forEach(function (item) {
+		item.addEventListener('click', function () {
+			let answer = item.nextElementSibling
+			if (item.closest('.faq__block').classList.contains('active')) {
+				answer.style.height = ''
+			} else {
+				answer.style.height = answer.scrollHeight + 'px'
+			}
+			item.closest('.faq__block').classList.toggle('active')
+		})
+	})
+}
+
 window.addEventListener('DOMContentLoaded', function() {
     videoOverlay()
-    stepsScroll( )
+    stepsScroll()
+    possibleTabs()
+    possibleCollapse()
+    reviewSwiper()
+    collapse()
 })
